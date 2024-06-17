7d4f104fcbce9026525068fb784b3bbea3032b4a
hondahayato <hayato810honda@gmail.com>
2024-06-17 10:46:47 +0900

    10-5

--- shooting/src/GameFrame.java
+++ shooting/src/GameFrame.java
@@ -1,4 +1,22 @@
+import java.util.Vector;
 
-public class GameFrame {
-
+public class GameFrame extends MyFrame{
+public void run() {
+	GameWorld.player=new Player(100,300,0,0);
+		addKeyListener(GameWorld.player);
+		GameWorld.playerBullet=new Vector<PlayerBullet>();
+		while(true) {
+			clear();
+			GameWorld.player.draw(this);
+			GameWorld.player.move();
+			int i=0;
+			while(i<GameWorld.playerBullet.size()) {
+				PlayerBullet b=GameWorld.playerBullet.get(i);
+				b.draw(this);
+				b.move();
+				i++;
+			}
+			sleep(0.03);
+		}
+	}
 }
--- shooting/src/Player.java
+++ shooting/src/Player.java
@@ -1,4 +1,44 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Player {
+public class Player extends Character implements KeyListener {
+	public Player(double x, double y, double vx, double vy) {
 
+		super(x, y, vx, vy);
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = -5;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 5;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_SPACE) {
+			GameWorld.playerBullet.add(
+					new PlayerBullet(x,y,0,-10));
+		}
+
+	}
+
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx = 0;
+		}
+	}
+
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	public void move() {
+		super.move();
+		if (x < 0)
+			x = 0;
+		if (x > 370)
+			x = 370;
+	}
 }
