8b476f71bff2043bcb9fe04e5b8a10c280d73031
hondahayato <hayato810honda@gmail.com>
2024-06-24 10:44:02 +0900

    12-2

--- shooting/src/GameFrame.java
+++ shooting/src/GameFrame.java
@@ -12,11 +12,14 @@ public void run() {
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
 			moveEnemies();
+			checkPlayerAndEnemies();
+			checkPlayerBulletsAndEnimies();
 			for (int i=0 ; i<GameWorld.enemies.size(); i++) {
 				Enemy e=GameWorld.enemies.get(i);
-				if (e.x==GameWorld.player.x &&
-				    e.y==GameWorld.player.y) {
+				if (Math.abs(e.x-GameWorld.player.x)<=30 &&
+				    Math.abs(e.y-GameWorld.player.y)<=30) {
 					System.out.println("やられた!");
+					GameWorld.player.y=-1000;
 				}
 			}
 			for (int i=0 ; i<GameWorld.enemies.size(); i++) {
@@ -43,6 +46,40 @@ public void run() {
     		 Enemy e=GameWorld.enemies.get(i);
     		 e.draw(this);
     		 e.move();
+    	 }
+     }
+     public void checkPlayerAndEnemies() {
+    	 for (int i=0 ; i<GameWorld.enemies.size(); i++) {
+				Enemy e=GameWorld.enemies.get(i);
+				if (Math.abs(e.x-GameWorld.player.x)<=30 &&
+				    Math.abs(e.y-GameWorld.player.y)<=30) {
+					System.out.println("やられた!");
+					GameWorld.player.y=-1000;
+				}
+			}
+     }
+     public void checkPlayerBulletsAndEnimies() {
+    	 int i=0;
+    	 while (i<GameWorld.playerBullets.size()) {
+    		 PlayerBullet b=GameWorld.playerBullets.get(i);
+    		 int j=0;
+    		 int hits=0;
+    		 while(j<GameWorld.enemies.size()) {
+    			 Enemy e=GameWorld.enemies.get(j);
+    			 if (Math.abs(e.x-b.x)<=30 &&
+    				 Math.abs(e.y-b.y)<=30) {
+    				 System.out.println("あたり");
+    				 hits++;
+    				 GameWorld.enemies.remove(j);
+    			 }else{
+    			 j++;
+    			 }
+    		 }
+    		 if (hits>0) {
+    			 GameWorld.playerBullets.remove(i);
+    		 }else {
+    		 }
+    		 j++;
     	 }
      }
 }
